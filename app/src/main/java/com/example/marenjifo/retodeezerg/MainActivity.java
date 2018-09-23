b36bff62c99d5e31a9911d60799910c27d0b5486
Maria Renjifo <marenjifo@MacBook-Pro-de-Maria-20.local>
2018-09-22 23:42:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/marenjifo/retodeezerg/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.marenjifo.retodeezerg;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView lv_listas;
+    ListaAdapter customAdapter;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lv_listas=findViewById(R.id.lv_listas);
+        customAdapter= new ListaAdapter(this);
+        lv_listas.setAdapter(customAdapter);
+
+
+
+        Lista lista1= new Lista("Musica Pop","marenjifo","10");
+        customAdapter.agregarLista(lista1);
+
+
+
+    }
+}
